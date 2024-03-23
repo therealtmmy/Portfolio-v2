681a4428029451a3c811e28ba065e55878a0e3ba
therealtmmy <oluwatimilehinsamson@gmail.com>
2024-03-24 00:23:20 +0100

    Buidling Sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-white text-[48px] font-bold">
+          Oluwatimilehin Esan
+        </h1>
+        <h3 className="text-white text-[25px] font-medium">
+          Junior Frontend Engineer
+        </h3>
+        <p className="text-[#b8b8b9] w-1/4">
+          I'm a motivated Front-End Developer passionate about creating
+          exceptional user experiences and translating UI designs into
+          pixel-perfect and accessible digital products for users.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/Components/Socials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Socials = () => {
+  return <div></div>;
+};
+
+export default Socials;
